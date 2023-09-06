57af2d87793c0f07123cddf8b4e978e127b543d5
Emre YARAŞIR <50424886+viowins@users.noreply.github.com>
2023-09-07 01:03:19 +0300

    push

--- app/home/Sections/Popular.jsx
+++ app/home/Sections/Popular.jsx
@@ -1,17 +1,6 @@
 import { Swiper, Card } from '@/components'
 
-async function getMovies() {
-  const res = await fetch(`${process.env.APP_URL}/api/moviedb`, { cache: 'no-store' })
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
-  return res.json()
-}
-
 export default async function Popular() {
-  const movies = await getMovies()
 
   return (
     <div>
