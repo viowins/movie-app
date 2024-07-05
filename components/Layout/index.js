d8691ca29b071ddb6754e3a6ac55dbe46710c8f7
EmreGuruDesk <50424886+viowins@users.noreply.github.com>
2024-07-05 20:57:09 +0300

    chore: Update main layout padding in Layout component

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
   return (
     <div className='grid grid-cols-1 max-w-[2000px] mx-auto overflow-x-hidden'>
       <Sidebar />
-      <main className='md:ml-[120px] pb-10 md:pb-6 p-6 2xl:px-0 min-h-full'>
+      <main className='md:ml-[120px] pb-10 md:pb-6 p-6 2xl:px-4 min-h-full'>
         {children}
         <Footer />
       </main>
